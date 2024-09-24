f064f0eda31933a1dc05d783946099a6be86db65
Олег Дидечкин <oleg.didechkin@gmail.com>
2024-09-24 20:32:02 +0300

    Removed hardcoded localhost

--- src/helpers/twitter.ts
+++ src/helpers/twitter.ts
@@ -1,2 +1,3 @@
-export const TWITTER_AUTH_REDIRECT_URL =
-  "http://localhost:3000/api/auth/twitter/callback";
+export const TWITTER_AUTH_REDIRECT_URL = `${
+  process.env.NEXT_PUBLIC_VERCEL_URL || "http://localhost:3000"
+}/api/auth/twitter/callback`;
