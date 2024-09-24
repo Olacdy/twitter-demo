173ecf1570fd10f3a12a7d4b76b8ff8e4f8e43fc
Олег Дидечкин <oleg.didechkin@gmail.com>
2024-09-24 20:42:14 +0300

    Improved auth redirect url

--- src/helpers/twitter.ts
+++ src/helpers/twitter.ts
@@ -1,3 +1,5 @@
 export const TWITTER_AUTH_REDIRECT_URL = `${
-  process.env.NEXT_PUBLIC_VERCEL_URL || "http://localhost:3000"
+  process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : "http://localhost:3000"
 }/api/auth/twitter/callback`;
